5d0358d8f4342c0341229171d0e3f44ed7b11fcc
Muhammad Zawawi <kembedt@gmail.com>
2021-12-07 22:56:47 +0700

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,21 +1,25 @@
 function pattern(n) {
-  // looping rows
-  for (let i = 0; i < n; i++) {
-    // Looping columns
-    for (let j = 0; j < n; j++) {
-      if (
-        i == 0 ||
-        j == 0 ||
-        i == j ||
-        i == n - 1 ||
-        j == n - 1 ||
-        i + j == n - 1
-      )
-        document.write(" * ");
-      else document.write(" # ");
+  if (n % 2 == 0) {
+    alert("Input must be Odd Number");
+  } else {
+    // looping rows
+    for (let i = 0; i < n; i++) {
+      // Looping columns
+      for (let j = 0; j < n; j++) {
+        if (
+          i == 0 ||
+          j == 0 ||
+          i == j ||
+          i == n - 1 ||
+          j == n - 1 ||
+          i + j == n - 1
+        )
+          document.write(" * ");
+        else document.write(" # ");
+      }
+      document.write("<br />");
     }
-    document.write("<br />");
   }
 }
 
-pattern(7);
+pattern(5);
